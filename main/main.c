794acafbfc2b80c3f93401f79c58d91f899b6533
HakeemS <33767804+HakeemS@users.noreply.github.com>
2024-01-16 10:06:49 -0500

    WaI; added blink-on-Rx task

--- main/main.c
+++ main/main.c
@@ -2,7 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "rom/uart.h" 
+#include "driver/gpio.h"
 
 
 #if CONFIG_FREERTOS_UNICORE
@@ -11,74 +11,104 @@
     static const BaseType_t app_cpu = 1; 
 #endif
 
-const char *msg = "Testing 1,2,3 ... Testing";
-char * TxPtr; 
+#define led_pin 13 
 
+const char *msg = " -- Task Messaging Demo --"; //remove at some point; random check
+char * TxPtr = NULL; 
+int TxFlag = 0; 
+
+
+//stores input onto heap; prints heap to confirm 
 void TaskOne(void *arg){
-    char *taskName = pcTaskGetName(NULL); 
-    char charArr[100] ; 
-    char *sCharArr = &charArr; 
+    //bool TxRdy = false; 
+    //char charArr[100] ; 
+    //char *sCharArr = &charArr; 
+    char *taskName = pcTaskGetName(NULL);
     uint8_t inChar;
     int count = 0; 
-    bool testPrint = false; 
-    char *TxArr = (char *)pvPortMalloc(100*sizeof(char)); 
-    TxPtr = TxArr; 
     while (1){
         inChar = fgetc(stdin); 
+        if (0 == TxFlag){
+            ESP_LOGI(taskName, "Enter Msg... "); 
+            TxPtr = (char *)pvPortMalloc(100*sizeof(char));                                                     //Default heap has space for 100 Chars
+            TxFlag = 1; 
+        }
         if ('\n' == inChar){
-            charArr[count] = inChar; 
-            count++; 
-            TxPtr = TxArr; 
-            testPrint = true;  
+            TxPtr[count] = inChar; 
+            count = 0;    
+            TxFlag = 2; 
+            ESP_LOGW(taskName, "Remaining mem in task stack (words): %d", uxTaskGetStackHighWaterMark(NULL));
+            ESP_LOGW(taskName, "Remaining mem on heap (bytes): %d\n", xPortGetFreeHeapSize());
+            //charArr[count] = inChar; 
         } else if (0xFF == inChar){
             ; 
         } else {
-            charArr[count] = inChar; 
-            TxArr[count] = inChar; 
+            TxPtr[count] = inChar; 
             count++; 
+            //charArr[count] = inChar; 
         }
-        
-        if (true == testPrint){
-            for (size_t i = 0; i < count; i++){
-                //printf("%c", charArr[i]);
-                //charArr[i] = ' '; 
-                //v2; above does same
-                printf("%c", *sCharArr); 
-                sCharArr++; 
-                charArr[i] = ' '; 
-            }
-            testPrint = false; 
-            count = 0; 
-            //v2; above does same  
-            sCharArr = &charArr;
-        }
-        ESP_LOGW(taskName, "Remaining mem in task stack (words): %d", uxTaskGetStackHighWaterMark(NULL));       // investigate how this changes when we go from 1800 to 1700 bytes of task stack
-        vTaskDelay(100/portTICK_PERIOD_MS); 
+        //will keep in code below commented out for potential future reference 
+        //if (true == TxRdy){
+        //    for (size_t i = 0; i < count; i++){
+        //        printf("%c", charArr[i]);
+        //        charArr[i] = ' '; 
+        //        ESP_LOGW(taskName, "Remaining mem in task stack (words): %d", uxTaskGetStackHighWaterMark(NULL));       // investigate how this changes when we go from 1800 to 1700 bytes of task stack
+        //        //v2; above does same
+        //        //printf("%c", *sCharArr); 
+        //        //sCharArr++; 
+        //        //charArr[i] = ' '; 
+        //    }
+        //    TxRdy = false; 
+        //    count = 0; 
+        //    //v2; above does same  
+        //    //sCharArr = &charArr;
+        //}
+        //
+        vTaskDelay(50/portTICK_PERIOD_MS); 
     }
     
     
 }
 
+//Task that prints input to serial and frees heap; prints heap afterwards to confirm 
 void TaskTwo(void *arg){
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    uint8_t testChar;  
     char *taskName = pcTaskGetName(NULL); 
     while (1){      
-        //ETS_STATUS s = uart_rx_one_char(&testChar); 
-        //if (NULL != testChar){
-        //    printf("%c, %d : T2\n", testChar, s); 
-        //}
-        //testChar = fgetc(stdin);
-        //if (0xFF != testChar){
-        //    printf("%c : T2\n", testChar); 
-        //} 
-        //ESP_LOGW(taskName, "Remaining mem in task stack (words): %d", uxTaskGetStackHighWaterMark(NULL));
-        for (size_t i = 0; i < 4; i++){
-            printf("%c\n", TxPtr[i]); 
+        if (2 == TxFlag){
+            int i = 0; 
+            char checkChar = ' '; 
+            ESP_LOGI(taskName, "Msg Received!");  
+            if (NULL != TxPtr){
+                while ('\n' != checkChar){
+                    checkChar = TxPtr[i]; 
+                    printf("%c", TxPtr[i]);
+                    i++; 
+                }  
+                vPortFree(TxPtr); 
+                ESP_LOGW(taskName, "Remaining mem on heap (bytes): %d", xPortGetFreeHeapSize());
+            }
+            TxFlag = 0;
         }
-        
-        vTaskDelay(10000/portTICK_PERIOD_MS); 
+        vTaskDelay(100/portTICK_PERIOD_MS); 
     }
+    
+}
+
+//Blinks onboard LED when Msg received
+void TaskThree(void *arg){
+    char lvl = 0; 
+    while (1){
+        if (2 == TxFlag){
+            lvl = 0; 
+            for (size_t i = 0; i < 11; i++){
+                vTaskDelay(75/portTICK_PERIOD_MS);                                        //Period = .1s; Duty cycle = 50%
+                gpio_set_level(led_pin, lvl); 
+                lvl = !lvl; 
+            }  
+        }
+        vTaskDelay(50/portTICK_PERIOD_MS); 
+    }
+    
     
 }
 
@@ -86,9 +116,12 @@ void setup(){
     vTaskDelay(1000/portTICK_PERIOD_MS); 
     char *taskName = pcTaskGetName(NULL); 
     printf("\n"); 
-    printf("%s: From setup\n", msg);
+    printf("%s\n \n", msg);
+    gpio_reset_pin(led_pin); 
+    gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);     
     xTaskCreatePinnedToCore(TaskOne, "Task One", 2000, NULL, 1, NULL, app_cpu); 
     xTaskCreatePinnedToCore(TaskTwo, "Task Two", 1800, NULL, 1, NULL, app_cpu);
+    xTaskCreatePinnedToCore(TaskThree, "TxRcvdBlink", 1024, NULL, 2, NULL, app_cpu); 
 }
 void app_main(void){
     setup(); 
